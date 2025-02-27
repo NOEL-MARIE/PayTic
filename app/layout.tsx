3c8dd07e82e9b50c12cb2bf4588b62e9aa259654
De Gonzague <comoenoelmariefrancois@gmail.com>
2025-02-27 10:47:16 +0000

    initalization of deployement

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,12 @@ import "./globals.css";
 // Déclaration de la police Inria Sans
 
 const inriaSans = localFont({
-  src: "@/app/Fonts/InriaSans-Light.ttf",
+  src: "@/app/Font/InriaSans-Light.ttf",
   variable: "--font-inria-sans",
   weight: "400",
 });
 const Inter = localFont({
-  src: "@/app/Fonts/Inter_18pt-Light.ttf",
+  src: "@/app/Font/Inter_18pt-Light.ttf",
   variable: "--font-Inter",
   weight: "400",
 });
