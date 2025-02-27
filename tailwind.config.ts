f7642e0c3ba3c0e114478af30660ebb8d3c6d2eb
De Gonzague <comoenoelmariefrancois@gmail.com>
2025-02-27 11:18:25 +0000

    modification of importation font method

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,26 +9,24 @@ export default {
   theme: {
     extend: {
       screens: {
-        'xs': '320px',  // 320px        
-        '2xl': '1440px', // Ajout d'un breakpoint pour 1440px
-
+        xs: "320px", // 320px
+        "2xl": "1440px", // Ajout d'un breakpoint pour 1440px
       },
       fontFamily: {
         // police perso
-        inriasans: ["--font-inria-sans "],
-        Inter: ["--font-Inter "],
-
+        inriasans: ["Inria Sans", "serif"],
+        Inter: ["Inter", "serif"],
       },
       colors: {
         paytic_primary: {
           DEFAULT: "#101828", // Correction de la syntaxe
         },
-        NavBar_col:"#0E315D",
+        NavBar_col: "#0E315D",
         text_navigation: "#454545",
         great_text: "#121212",
         small_text: "#4F4F4F",
         feature: "#F6F6F6",
-        btn_col: "#007AFF" // Suppression de la virgule finale qui causait une erreur
+        btn_col: "#007AFF", // Suppression de la virgule finale qui causait une erreur
       },
     },
   },
